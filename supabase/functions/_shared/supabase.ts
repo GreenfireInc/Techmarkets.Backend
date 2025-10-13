32b9aced749e2539e5b983fbfcff27303b984242
Aciel Ochoa <aceiscoding@gmail.com>
2025-10-13 13:20:34 -0500

    created getServiceRoleClient method for creating Service Role instance of supabase client for admin privileges

--- supabase/functions/_shared/supabase.ts
+++ supabase/functions/_shared/supabase.ts
@@ -1,13 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
 
-export function getClient(authorizationHeader: any) {
-  // Initialize Supabase client
+export function getServiceRoleClient() {
+  // Initialize Service Role Supabase client
   const supabaseUrl = Deno.env.get('SUPABASE_URL')!
   const supabaseServiceKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
-  const supabase = createClient(supabaseUrl, supabaseServiceKey, {
-    global: {
-      headers: { Authorization: authorizationHeader }
-    }
-  })
+  const supabase = createClient(supabaseUrl, supabaseServiceKey)
+  return supabase
+}
+
+export function getClient(token?: string) {
+  // Initialize Supabase client
+  const supabaseUrl = Deno.env.get('SUPABASE_URL')!
+  const supabase = createClient(supabaseUrl, token)
   return supabase
 }
